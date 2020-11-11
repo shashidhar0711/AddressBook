727c44767fd60347eba1d77d007d8cb94ffa39ae
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-11 17:03:57 +0530

    [sasic] Add . Adding Contacts into AddressBook Text File

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,9 +4,17 @@ namespace AddressBook
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Printing Welcome Message
+            Console.WriteLine("Welcome to Address Book");
+            /// Creating a Reference of AddressBook
+            AddressBook addressBook = new AddressBook();
+            addressBook.BookManagementView();
         }
     }
 }
