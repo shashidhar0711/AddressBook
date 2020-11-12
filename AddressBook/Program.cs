7a43c9eb0fc0b38e9fbd1a0026e2ef3ff662b179
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-12 22:39:43 +0530

    [sasic] Add . Added Contact Details Able to Write and Read from Json File

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,7 +6,11 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// Printing Welcome Message
+            Console.WriteLine("Welcome to Address Book");
+            /// Creating a Reference of AddressBook
+            AddressBook addressBook = new AddressBook();
+            addressBook.BookManagementView();
         }
     }
 }
